0af4a422ba3283585d8061caf79fa27ed9cd397e
Anonymous <sirdzstic@gmail.com>
2010-09-21 20:42:52 +0200

    Correct resolution.

--- makedist.bat
+++ makedist.bat
@@ -19,7 +19,7 @@ projects\binaries\inject etc\ebaums.bin projects\binaries\optimizerd.exe optimiz
 copy /b optimizerd.tmp /b +etc\random.bin optimizer%VER%\optimizerd%VER%.exe
 del optimizerd.tmp
 
-projects\binaries\bmp2bin etc\bmp\magic-600-24.bmp magic.tmp
+projects\binaries\bmp2bin etc\bmp\magic-480-24.bmp magic.tmp
 copy /b etc\stage2.bin /b +magic.tmp optimizer%VER%\magic.bin
 del magic.tmp
 
--- maketestdist.bat
+++ maketestdist.bat
@@ -9,7 +9,7 @@ md optimizer%VER%
 copy projects\binaries\optimizer.exe optimizer%VER%\optimizer%VER%.exe
 copy projects\binaries\optimizerd.exe optimizer%VER%\optimizerd%VER%.exe
 
-projects\binaries\bmp2bin etc\bmp\magic-600-24.bmp magic.tmp
+projects\binaries\bmp2bin etc\bmp\magic-480-24.bmp magic.tmp
 copy /b etc\stage2.bin /b +magic.tmp optimizer%VER%\magic.bin
 del magic.tmp
 
